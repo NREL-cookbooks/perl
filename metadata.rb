6653e32c3e517233f1bee5d907c3c48e433d1b48
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-12-08 19:48:48 +0000

    Add DBD::Pg recipe for perl.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs perl and provides a define for maintaining CPAN modules"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.10.2"
+version           "0.10.3"
 
 recipe "perl", "Installs perl and provides a script to install cpan modules"
 
