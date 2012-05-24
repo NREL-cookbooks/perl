a80b3b8008afafbbc60880d869d2af71861f9d38
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-05-24 04:23:19 +0000

    Fix environment variable setting to work with the default cpan_module installer.

--- recipes/dbd_oracle.rb
+++ recipes/dbd_oracle.rb
@@ -19,15 +19,13 @@ dependencies.each do |package_name|
   package package_name
 end
 
+ENV["LD_LIBRARY_PATH"] = node[:oracle_instantclient][:path]
+
+# LD_RUN_PATH seems to be needed for DBD::Oracle to work through Apache on
+# Redhat.
+ENV["LD_RUN_PATH"] = node[:oracle_instantclient][:path]
+
 cpan_module "DBD::Oracle" do
   # Tests fail, since it can't connect to a dummy Oracle database, but we don't care.
   force true
-
-  environment({
-    "LD_LIBRARY_PATH" => node[:oracle_instantclient][:path],
-
-    # LD_RUN_PATH seems to be needed for DBD::Oracle to work through Apache on
-    # Redhat.
-    "LD_RUN_PATH" => node[:oracle_instantclient][:path],
-  })
 end
